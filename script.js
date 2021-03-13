f55bd38fdbc3bb662f5bfd2fad76b398af5d20eb
Kunal Kp <kp_1@outlook.com>
2021-03-13 09:26:43 +0400

    JS updates:

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var config = {
     behavior: 'scroll'
 };
 
+var ticker = Tickit(config).init();
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
