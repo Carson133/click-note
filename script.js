b347503bb2336d6885c3b7a0ffbabe7d558e2c08
Kunal Kp <kp_1@outlook.com>
2021-03-13 14:58:30 +0400

    CSS background updates

--- script.js
+++ script.js
@@ -1,193 +1,158 @@
-var config = {
-    data: ['item 1', 'item 2', 'item 3'],
-    selector: '#tickit',
-    duration: 1000,
-    initialPos: -15,
-    behavior: 'scroll'
-};
-
-var ticker = Tickit(config).init();
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-/* global define */
-/**
- * @name tickit
- * @overview A basic ticker that uses CSS animations & vanilla JavaScript.
- * @version 0.0.0
- * @author Jon Chretien
- * @license MIT
- */
-(function () {
-  /**
-   * @param {Object} config - Tickit configuration values.
-   * @param {array} config.data - The text to display.
-   * @param {string} config.selector - The id for the container element.
-   * @param {number} config.duration - The animation duration.
-   * @param {number} config.initialPos - The initial offset position.
-   * @param {string} config.behavior - The user interaction behavior.
-   */
-  var Tickit = function Tickit(config) {
-    var isString = function isString(val) {
-      return toString.call(val) === '[object String]';
-    }; // underscore.js
-    var isNumber = function isNumber(val) {
-      return toString.call(val) === '[object Number]';
-    }; // underscore.js
-    var logError = function logError(type) {
-      throw new Error('Expecting a ' + type);
-    };
-    var setTransform = function setTransform(position) {
-      return 'translate3d(0, ' + position + 'px, 0)';
-    };
-
-    if (!Array.isArray(config.data)) logError('array');
-    if (!isString(config.behavior) || !isString(config.selector)) logError('string');
-    if (!isNumber(config.duration) || !isNumber(config.initialPos)) logError('number');
-
-    var options = _extends({}, config);
-    var data = options.data;
-    var duration = options.duration;
-    var behavior = options.behavior;
-    var initialPos = options.initialPos;
-    var selector = options.selector;
-
-    var tickit = document.querySelector(selector);
-    var tickitInner = tickit.querySelector('.js-tickit-inner');
-    var transitionIn = 0;
-    var transitionOut = -initialPos;
-    var classNames = ['tickit-text', 'js-tickit-text'];
-
-    var counter = 0;
-    var isAnimating = false;
-    var isClickActivated = false;
-    var isTickitVisible = false;
-    var tickitText = null;
-
-    /**
-     * Hides text container.
-     */
-    function hideTickit() {
-      isAnimating = true;
-      isTickitVisible = false;
-      tickitText.style.transform = setTransform(transitionOut);
-    }
-
-    /**
-     * Reveals text container.
-     */
-    function showTickit() {
-      isAnimating = true;
-      isTickitVisible = true;
-      tickitText.style.transform = setTransform(transitionIn);
-    }
-
-    /**
-     * Handles animation frame based on behavior.
-     */
-    function draw() {
-      var timer = setTimeout(function () {
-        if (!isClickActivated && isTickitVisible && behavior === 'click' || isTickitVisible && isClickActivated) {
-          isAnimating = false;
-          clearTimeout(timer);
-          return;
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
         }
+    });
+});
 
-        if (!isTickitVisible) {
-          tickitText.textContent = data[counter++ % data.length];
-          showTickit();
-          return;
+var previousNotes = [];
+
+function appendNoteAndDate(){
+    $.ajax({
+        url: "http://worldtimeapi.org/api/timezone/America/Los_Angeles" ,
+        method: "GET"
+    }) .then(function (response){
+        let time = response.datetime.slice(0,16)
+        let input = $('#note-area').val();
+    
+        function keyNumber(){
+            if(localStorage.length === 0){
+                return '0';
+            } else {
+                return localStorage.length / 2;
+            } 
         }
+        let key = keyNumber();
 
-        if (isTickitVisible) {
-          hideTickit();
+        console.log(key)
+        console.log(keyNumber())
+
+        localStorage.setItem(key, input)
+        localStorage.setItem('time'+key, time.replace('T',' '))
+
+        $(".list").append($("<p>").attr('class', 'list-items').attr('id', key).html(time.replace('T',' ')+" | "+ input));
+    })
+}
+
+for(let i = 0; i < localStorage.length / 2; i++){
+    let localTime = localStorage.getItem('time'+i.toString())
+    let localInput = localStorage.getItem(i.toString())
+
+    $(".list").append($("<p>").attr('class', 'list-items').attr('id', i.toString()).html(localTime +" | "+ localInput));
+}
+
+$('#submit-button').on("click", function() {
+    appendNoteAndDate();
+})
+
+var previousNotes = [];
+
+function appendNoteAndDate(){
+    $.ajax({
+        url: "http://worldtimeapi.org/api/timezone/America/Los_Angeles" ,
+        method: "GET"
+    }) .then(function (response){
+        let time = response.datetime.slice(0,16)
+        let input = $('#note-area').val();f
+    
+        function keyNumber(){
+            if(localStorage.length === 0){
+                return '0';
+            } else {
+                return localStorage.length / 2;
+            } 
         }
-      }, duration);
-    }
+        let key = keyNumber();
 
-    /**
-     * Adds text element.
-     */
-    function addText() {
-      var el = document.createElement('div');
-      el.className = classNames.join(' ');
-      el.style.transform = setTransform(initialPos);
-      tickitInner.appendChild(el);
-      tickitText = tickitInner.querySelector('.' + classNames[1]);
-    }
+        console.log(key)
+        console.log(keyNumber())
 
-    /**
-     * Removes text element.
-     */
-    function removeText() {
-      tickitInner.removeChild(tickitText);
-    }
+        localStorage.setItem(key, input)
+        localStorage.setItem('time'+key, time.replace('T',' '))
 
-    /**
-     * Handles click events.
-     *
-     * @param {Object} event - The event triggered.
-     */
-    function handleClickEvent(event) {
-      if (!isAnimating && event.target && event.target.nodeName.toLowerCase() === 'div') {
-        isClickActivated = true;
-        hideTickit();
-        return;
-      }
-    }
+        $(".list").append($("<p>").attr('class', 'list-items').attr('id', key).html(time.replace('T',' ')+" | "+ input));
+    })
+}
 
-    /**
-     * Handles transition end events.
-     *
-     * @param {Object} event - The event triggered.
-     */
-    function handleTransitionEndEvent(event) {
-      if (event.target && event.target.nodeName.toLowerCase() === 'div') {
-        if (!isTickitVisible) {
-          removeText();
-          addText();
+for(let i = 0; i < localStorage.length / 2; i++){
+    let localTime = localStorage.getItem('time'+i.toString())
+    let localInput = localStorage.getItem(i.toString())
+
+    $(".list").append($("<p>").attr('class', 'list-items').attr('id', i.toString()).html(localTime +" | "+ localInput));
+}
+
+
+$('#submit-button').on("click", function() {
+    appendNoteAndDate();
+})
+
+// Weather 
+
+var previousNotes = [];
+
+function appendNoteAndDate(){
+    $.ajax({
+        url: "http://worldtimeapi.org/api/timezone/America/Los_Angeles" ,
+        method: "GET"
+    }) .then(function (response){
+        let time = response.datetime.slice(0,16)
+        let input = $('#note-area').val();
+    
+        function keyNumber(){
+            if(localStorage.length === 0){
+                return '0';
+            } else {
+                return localStorage.length / 2;
+            } 
         }
+        let key = keyNumber();
 
-        requestAnimationFrame(draw);
-      }
+        console.log(key)
+        console.log(keyNumber())
+
+        localStorage.setItem(key, input)
+        localStorage.setItem('time'+key, time.replace('T',' '))
+
+        $(".list").append($("<p>").attr('class', 'list-items').attr('id', key).html(time.replace('T',' ')+" | "+ input));
+    })
+}
+
+for(let i = 0; i < localStorage.length / 2; i++){
+    let localTime = localStorage.getItem('time'+i.toString())
+    let localInput = localStorage.getItem(i.toString())
+
+    $(".list").append($("<p>").attr('class', 'list-items').attr('id', i.toString()).html(localTime +" | "+ localInput));
+}
+$('#submit-button').on("click", function() {
+    appendNoteAndDate();
+})
+$(document).ready(function(){
+    function getLocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(getWeather);
+        }else{
+            alert("Geolocation not supported by this browser");
+        }
     }
 
-    /**
-     * Attaches event handlers.
-     *
-     * @api private
-     */
-    function attachEvents() {
-      if (behavior === 'click') {
-        tickitInner.addEventListener('click', handleClickEvent, false);
-      }
-      tickitInner.addEventListener('transitionend', handleTransitionEndEvent, false);
-    }
+function getWeather(position){
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    let API_KEY = "3c08c223f7924790dbebee106b70e779";
+    let baseURL = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${API_KEY}`;
 
-    /**
-     * Initialize logic.
-     *
-     * @api private
-     */
-    function init() {
-      attachEvents();
-      addText();
-      draw();
-    }
+    $.get(baseURL,function(res){
+        let data = res.current;
+        let temp = Math.floor(data.temp - 273);
+        let condition = data.weather[0].description;
 
-    return { init: init };
-  };
+        $('#temp-main').html(`${temp}°`);
+        $('#condition').html(condition);
+    })    
+}
 
-  /**
-   * Expose `Tickit`.
-   */
-  if (typeof define === 'function' && define.amd) {
-    define(Tickit);
-  } else if ((typeof module === 'undefined' ? 'undefined' : _typeof(module)) === 'object' && module.exports) {
-    module.exports = Tickit;
-  } else {
-    window.Tickit = Tickit;
-  }
-})();
+getLocation();
+})
